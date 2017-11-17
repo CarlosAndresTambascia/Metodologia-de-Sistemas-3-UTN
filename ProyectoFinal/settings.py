df16278fa04f39d9420b0de7db77596b214aa11b
Schultena <alejandroschulten@gmail.com>
2017-11-17 16:38:02 -0300

    algo

--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#MEDIA_ROOT = '/media'
+
+#MEDIA_URL = 'http://127.0.0.1:8000/media/'
--- alquileres/models.py
+++ alquileres/models.py
@@ -7,12 +7,14 @@ from django.contrib.auth.models import User
 
 class Ciudad(models.Model):
     nombre = models.CharField(max_length=200, unique=True)
+    def __str__(self):
+        return self.nombre
 
 
 class Propiedad(models.Model):
     descripcion = models.CharField(max_length=500)
     precioDiario = models.DecimalField(max_digits=5, decimal_places=2)
-    imagen = models.ImageField()
+    imagen = models.ImageField(upload_to='image', max_length=100)
     titulo = models.CharField(max_length=50)
     numeroFicha = models.IntegerField()
     maximoHabitantes = models.IntegerField()
